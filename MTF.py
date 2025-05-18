a4194c21e1618c3e4114b2042bbe6e2353cc71ae
gash666 <120337165+gash666@users.noreply.github.com>
2025-05-18 19:55:58 +0300

    mtf_decode done

--- MTF.py
+++ MTF.py
@@ -1,6 +1,17 @@
 def mtf_decode(encoded_string, alphabet: str):
-	length = 5  # Get length
+	length = len(encoded_string)
 	answer = ''
 	for i in range(length):
-		index = 5  # Read index
-		answer += alphabet[]
+		index = encoded_string[i]
+		answer += alphabet[index]
+		alphabet = alphabet[index] + alphabet[:index] + alphabet[index + 1:]
+	return answer
+
+
+def main():
+	print(mtf_decode("bananaaa", "abcdefghijklmnopqrstuvwxyz"))
+	print(mtf_decode([1, 1, 13, 1, 1, 1, 0, 0], "abcdefghijklmnopqrstuvwxyz"))
+
+
+if __name__ == "__main__":
+	main()
