8429a1078c61a2e158023709eeb47e0e51c84297
ItamarACGN <Itamarnir10@gmail.com>
2025-05-18 19:57:52 +0300

    implementedd bwt_encode

--- MTF.py
+++ MTF.py
@@ -1,3 +1,11 @@
+def mtf_encode(encoded_string, alphabet: str):
+	ans = []
+	for c in encoded_string:
+		idx = alphabet.find(c)
+		ans.append(idx)
+		alphabet = c+alphabet[0:idx]+alphabet[idx+1:len(alphabet)]
+	return ans
+
 def mtf_decode(encoded_string, alphabet: str):
 	length = len(encoded_string)
 	answer = ''
@@ -7,23 +15,13 @@ def mtf_decode(encoded_string, alphabet: str):
 		alphabet = alphabet[index] + alphabet[:index] + alphabet[index + 1:]
 	return answer
 
-
 def main():
-	print(mtf_decode("bananaaa", "abcdefghijklmnopqrstuvwxyz"))
+	print(mtf_encode("bananaaa", "abcdefghijklmnopqrstuvwxyz"))
 	print(mtf_decode([1, 1, 13, 1, 1, 1, 0, 0], "abcdefghijklmnopqrstuvwxyz"))
 
 
 if __name__ == "__main__":
 	main()
 
-		index = 5  # Read index
-		answer += alphabet[]
 
-def mtf_encode(encoded_string, alphabet: str):
-	ans = []
-	for c in encoded_string:
-		idx = alphabet.find(c)
-		ans.append(idx)
-		alphabet = c+alphabet[0:idx]+alphabet[idx+1:len(alphabet)]
-	return ans
 
