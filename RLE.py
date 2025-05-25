ca7c1d7292753eeda19dd50e2a6d25b66d0d7017
ItamarACGN <Itamarnir10@gmail.com>
2025-05-25 18:32:38 +0300

    implementedd bwt_encode

--- RLE.py
+++ RLE.py
@@ -1,8 +1,7 @@
-
-def RLE_encode(original_string):
+def rle_encode(original_string):
     counter = 0
     ans = []
-    original_string.append("a")
+    original_string+="a"
     for i in range(0,len(original_string)-1):
         counter+=1
         if original_string[i] != original_string[i+1]:
@@ -11,3 +10,17 @@ def RLE_encode(original_string):
             counter = 0
     return ans
 
+
+
+
+def rle_decode(encoded_list):
+	answer = []
+	for i in range(0, len(encoded_list), 2):
+		answer += encoded_list[i] * [encoded_list[i + 1]]
+	return answer
+
+def main():
+	print(rle_encode( [1,1,1,0,3,5]))
+
+if __name__ == "__main__":
+    main()
