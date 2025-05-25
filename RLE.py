c89183df3f6cbd010b316c14b58ece202d02830d
gash666 <120337165+gash666@users.noreply.github.com>
2025-05-25 18:42:17 +0300

    a

--- RLE.py
+++ RLE.py
@@ -1,9 +1,9 @@
 def rle_encode(original_string):
     counter = 0
     ans = []
-    original_string+="a"
+    original_string += "a"
     for i in range(0,len(original_string)-1):
-        counter+=1
+        counter += 1
         if original_string[i] != original_string[i+1]:
             ans.append(counter)
             ans.append(original_string[i])
@@ -19,9 +19,11 @@ def rle_decode(encoded_list):
 		answer += encoded_list[i] * [encoded_list[i + 1]]
 	return answer
 
+
 def main():
-	print(rle_encode([[1, 1, 1, 2, 3, 5, 4, 5, 5, 5, 5]]))
-	print(rle_decode(rle_encode([[1, 1, 1, 2, 3, 5, 4, 5, 5, 5, 5]])))
+	print(rle_encode([1, 1, 1, 2, 3, 5, 4, 5, 5, 5, 5]))
+	print(rle_decode(rle_encode([1, 1, 1, 2, 3, 5, 4, 5, 5, 5, 5])))
+
 
 if __name__ == "__main__":
-    main()
+	main()
